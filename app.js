bc77a0786703cc607413488e18f86cb161df9238
Fredric McWilliams <fredmcwilliams92@gmail.com>
2021-05-12 18:31:15 -0500

    adding in functionlity to publish html page using node

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const profileDataArgs = process.argv.slice(2);
+// used to get the user input
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// used to store the user input
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-}
+// used to post the user input
+const generatePage = (userName, githubName) => {
+return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
 
-    console.log('================');
-
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+    <body>
+        <h1>${name}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
 };
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output');
+});
+
